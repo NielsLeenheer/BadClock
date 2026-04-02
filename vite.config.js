8fb674c944e26ced392948a842eec38eea8bd00d
Niels Leenheer <niels@leenheer.nl>
2026-04-02 13:56:57 +0200

    Add server configuration to Vite for port specification

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import { viteSingleFile } from 'vite-plugin-singlefile';
 
 export default defineConfig({
   plugins: [viteSingleFile()],
+  server: {
+    port: 0,
+  },
   build: {
     outDir: 'dist',
     emptyOutDir: true,
